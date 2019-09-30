ad6f193445a560fc8c7d6806f06b85b28739dafd
gness1804 <graham.nessler@gmail.com>
2019-09-30 11:35:03 -0500

    WIP: create draft of [city].svelte

---
+++ src/routes/cities/[city].svelte
@@ -0,0 +1,19 @@
+<script context="module">
+  export async function preload({ params }) {
+    const res = await this.fetch(`cities/${params.city}.json`);
+    const data = await res.json();
+
+    if (res.status === 200) {
+      return { data };
+    }
+    return this.error(res.status, data.message);
+  }
+</script>
+
+<script>
+  export let data;
+</script>
+
+<!-- <svelte:head>
+  <title>{cityName} Weather</title>
+</svelte:head> -->
