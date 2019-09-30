d8e04537fb52dd1ace86b172a5c3108e5775d02e
gness1804 <graham.nessler@gmail.com>
2019-09-30 15:27:37 -0500

    Add attribution link to home

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,8 +4,15 @@
 
 <div id="app">
   <h1
-    class="text-center my-0 mx-auto text-5xl uppercase font-bold mb-2
+    class="text-center my-0 mx-auto text-5xl uppercase font-bold mb-10
     xs:text-6xl">
     Fair Weather
   </h1>
+
+  <a
+    class="block text-xl text-center text-blue-600 hover:text-blue-400"
+    href="https://darksky.net/poweredby/"
+    target="_blank">
+    Powered by Dark Sky
+  </a>
 </div>
