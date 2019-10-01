49484d0c4a67b781ec0da056c55a823f679b0a6b
gness1804 <graham.nessler@gmail.com>
2019-10-01 13:58:42 -0500

    Succeeded in prettifying the json files for cities

--- src/scripts/seedCityData.js
+++ src/scripts/seedCityData.js
@@ -2,11 +2,13 @@
 /* eslint-disable no-await-in-loop */
 require('dotenv').config();
 const { writeFile } = require('fs');
+const { exec } = require('child_process');
 const util = require('util');
 const axios = require('axios');
 const links = require('../routes/cities/_cityLinks');
 
 const promisifiedWriteFile = util.promisify(writeFile);
+const promisifiedExec = util.promisify(exec);
 
 const seedData = async () => {
   // eslint-disable-next-line no-unused-vars
@@ -16,13 +18,12 @@ const seedData = async () => {
       slug: city,
     } = link;
 
+    const file = `./src/routes/cities/_cityStaticData/${city}.json`;
     const data = await axios.get(
       `https://api.darksky.net/forecast/${process.env.DARK_SKY_KEY}/${lat},${lng}`,
     );
-    promisifiedWriteFile(
-      `./src/routes/cities/_cityStaticData/${city}.json`,
-      JSON.stringify({ data: data.data }),
-    );
+    await promisifiedWriteFile(file, JSON.stringify({ data: data.data }));
+    await promisifiedExec(`npm run prettier:spec ${file}`);
   }
   return true;
 };
