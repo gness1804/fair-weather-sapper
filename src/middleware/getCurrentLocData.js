8167bdf5de060a66521b8e38e2afa7c7f8568971
gness1804 <graham.nessler@gmail.com>
2019-10-02 14:41:14 -0500

    Improve geolocation data call by adding more messages and error handling

--- src/middleware/getCurrentLocData.js
+++ src/middleware/getCurrentLocData.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 require('dotenv').config();
 
 const fetch = require('node-fetch');
@@ -9,18 +10,33 @@ const getData = async (req, res) => {
     res.writeHead(200, {
       'Content-Type': 'application/json',
     });
+    console.info(
+      "Not making call to Dark Sky for user's current location because testing mode is on.",
+    );
     res.end(JSON.stringify({}));
     return;
   }
   const { lat, lng } = req.body;
   const url = `https://api.darksky.net/forecast/${process.env.DARK_SKY_KEY}/${lat},${lng}`;
-  const _res = await fetch(url);
-  const text = await _res.text();
-  const currentTemp = JSON.parse(text).currently.temperature;
-  res.writeHead(200, {
-    'Content-Type': 'application/json',
-  });
-  res.end(JSON.stringify({ temp: currentTemp }));
+  try {
+    const _res = await fetch(url);
+    const text = await _res.text();
+    const currentTemp = JSON.parse(text).currently.temperature;
+    res.writeHead(200, {
+      'Content-Type': 'application/json',
+    });
+    res.end(JSON.stringify({ temp: currentTemp }));
+  } catch (error) {
+    res.writeHead(500, {
+      'Content-Type': 'application/json',
+    });
+    console.error(`Error fetching user's weather data: ${error}`);
+    res.end(
+      JSON.stringify({
+        message: error.message || error,
+      }),
+    );
+  }
 };
 
 module.exports = getData;
