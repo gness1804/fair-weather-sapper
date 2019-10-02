ecd99b4cf1886b5f3829cd9706fee636aef9e04d
gness1804 <graham.nessler@gmail.com>
2019-10-02 09:13:42 -0500

    Add mocha and add npm run unit to preCommit

--- preCommit.js
+++ preCommit.js
@@ -99,6 +99,7 @@ getCurrentBranch()
   .then(() => checkForWarnings())
   .then(() => promisifiedExec('npm run prettier'))
   .then(() => promisifiedExec('npm run lint'))
+  .then(() => promisifiedExec('npm run unit'))
   .then(() => promisifiedExec('npm run e2e'))
   .then(() => {
     process.stdout.write('Successfully passed pre-commit checks. \n');
