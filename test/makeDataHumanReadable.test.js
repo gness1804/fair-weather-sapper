68e9a39b01cd96e665625213ea6ec35b570b7b16
gness1804 <graham.nessler@gmail.com>
2019-10-02 09:20:48 -0500

    Add test for returns the correct value

--- test/makeDataHumanReadable.test.js
+++ test/makeDataHumanReadable.test.js
@@ -7,4 +7,10 @@ describe('makeDateHumanReadable', () => {
   it('should be a function', () => {
     assert(App instanceof Function);
   });
+
+  it('returns the correct value', () => {
+    const timestamp = 1569932805;
+    const result = App(timestamp);
+    assert.strictEqual(result, '7:26 AM');
+  });
 });
