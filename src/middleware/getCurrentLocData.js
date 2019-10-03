26cf64897d0084442f407a7f90785bf64d3bc060
gness1804 <graham.nessler@gmail.com>
2019-10-03 13:15:57 -0500

    Add stub for my weather api call

--- src/middleware/getCurrentLocData.js
+++ src/middleware/getCurrentLocData.js
@@ -1,22 +1,50 @@
-/* eslint-disable no-console */
 require('dotenv').config();
 
 const fetch = require('node-fetch');
+const { readFile } = require('fs');
+const util = require('util');
+
+const promisifiedReadFile = util.promisify(readFile);
 
 const isTesting = process.env.TESTING === 'true';
 
 const getData = async (req, res) => {
-  if (isTesting) {
-    res.writeHead(200, {
-      'Content-Type': 'application/json',
-    });
-    console.info(
-      "Not making call to Dark Sky for user's current location because testing mode is on.",
-    );
-    res.end(JSON.stringify({}));
-    return;
-  }
   const { lat, lng } = req.body;
+  if (isTesting) {
+    try {
+      const _res = JSON.parse(
+        await promisifiedReadFile(
+          './src/routes/cities/_cityStaticData/austin.json',
+          'utf-8',
+        ),
+      );
+
+      const { temperature: currentTemp } = _res.data.currently;
+      const { summary } = _res.data.currently;
+
+      res.writeHead(200, {
+        'Content-Type': 'application/json',
+      });
+      // eslint-disable-next-line no-console
+      console.info('Made call for weather data using JSON stub.');
+      res.end(JSON.stringify({ temp: currentTemp, summary }));
+      return;
+    } catch (error) {
+      res.writeHead(500, {
+        'Content-Type': 'application/json',
+      });
+      // eslint-disable-next-line no-console
+      console.error(
+        `Error fetching weather data from flat JSON file: ${error}`,
+      );
+      res.end(
+        JSON.stringify({
+          message: error.message || error,
+        }),
+      );
+      return;
+    }
+  }
   const url = `https://api.darksky.net/forecast/${process.env.DARK_SKY_KEY}/${lat},${lng}`;
   try {
     const _res = await fetch(url);
@@ -32,6 +60,7 @@ const getData = async (req, res) => {
     res.writeHead(500, {
       'Content-Type': 'application/json',
     });
+    // eslint-disable-next-line no-console
     console.error(`Error fetching user's weather data: ${error}`);
     res.end(
       JSON.stringify({
