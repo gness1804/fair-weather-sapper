2ee26d0e0d35d62dfad5db82aa6abce27fc52867
gness1804 <graham.nessler@gmail.com>
2019-10-11 14:39:16 -0500

    Create new WeatherDisplay component

--- src/routes/cities/[city].svelte
+++ src/routes/cities/[city].svelte
@@ -11,7 +11,9 @@
 </script>
 
 <script>
-  export let data;
+  import WeatherDisplay from '../../components/WeatherDisplay.svelte';
+
+  export let data = {};
 </script>
 
 <svelte:head>
@@ -21,62 +23,5 @@
 <div class="text-center">
   <h2 class="font-bold text-gray-600 text-3xl mb-8">{data.name} Weather</h2>
 
-  <img
-    src={data.iconSrc}
-    alt={data.icon}
-    title={data.icon}
-    class="my-0 mx-auto h-32 w-32" />
-
-  <p class={`text-5xl text-${data.currentTempColor} mb-6`}>
-    <span class="current-temp">{data.currentTemp}</span>
-    &deg; F
-  </p>
-
-  <p class="current-conditions-message mb-6">
-    The weather is {data.summary} with a {Math.round(data.precipProbability)}%
-    chance of rain.
-  </p>
-
-  <div class="today-container my-0 mx-auto w-most md:w-half">
-    <h3 class="text-2xl mb-6">Today:</h3>
-
-    <div class="high-low-temps-container flex justify-between mb-6">
-      <div class="high-temp flex items-center justify-center">
-        <div class="h-6 w-6 mr-4">
-          <img src="arrow-up.png" alt="Up arrow." />
-        </div>
-        <span class={`text-${data.dailyHighTempColor} text-2xl`}>
-          <span class="high-temp">{data.dailyHighTemp}</span>
-          &deg;
-        </span>
-      </div>
-      <div class="low-temp flex items-center justify-center">
-        <div class="h-6 w-6 mr-4">
-          <img src="arrow-down.png" alt="Down arrow." />
-        </div>
-        <span class={`text-${data.dailyLowTempColor} text-2xl`}>
-          <span class="low-temp">{data.dailyLowTemp}</span>
-          &deg;
-        </span>
-      </div>
-    </div>
-
-    <div class="sunrise-sunset-container flex justify-between">
-      <div class="sunrise flex items-center justify-center">
-        <div class="h-10 w-10 mr-2 md:mr-4">
-          <img src="sunrise.png" alt="Sun rising." />
-        </div>
-        <span class="sunrise-time text-base md:text-xl">
-          {data.sunriseTime}
-        </span>
-      </div>
-      <div class="sunset flex items-center justify-center">
-        <div class="h-10 w-10 mr-2 md:mr-4">
-          <img src="sunset.png" alt="Sun setting." />
-        </div>
-        <span class="sunset-time text-base md:text-xl">{data.sunsetTime}</span>
-      </div>
-    </div>
-
-  </div>
+  <WeatherDisplay {data} />
 </div>
