8a972edd7b8bc463d452cbe45c6c9e37794c91d6
gness1804 <graham.nessler@gmail.com>
2019-10-11 14:51:57 -0500

    Create SunriseSunsetContainer and HighLowTempsContainer as two new components

---
+++ src/components/SunriseSunsetContainer.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let sunriseTime;
+  export let sunsetTime;
+</script>
+
+<div class="sunrise-sunset-container flex justify-between">
+  <div class="sunrise flex items-center justify-center">
+    <div class="h-10 w-10 mr-2 md:mr-4">
+      <img src="sunrise.png" alt="Sun rising." />
+    </div>
+    <span class="sunrise-time text-base md:text-xl">{sunriseTime}</span>
+  </div>
+  <div class="sunset flex items-center justify-center">
+    <div class="h-10 w-10 mr-2 md:mr-4">
+      <img src="sunset.png" alt="Sun setting." />
+    </div>
+    <span class="sunset-time text-base md:text-xl">{sunsetTime}</span>
+  </div>
+</div>
--- src/components/WeatherDisplay.svelte
+++ src/components/WeatherDisplay.svelte
@@ -1,4 +1,7 @@
 <script>
+  import HighLowTempsContainer from './HighLowTempsContainer.svelte';
+  import SunriseSunsetContainer from './SunriseSunsetContainer.svelte';
+
   export let data = {};
 </script>
 
@@ -22,43 +25,15 @@
   <div class="today-container my-0 mx-auto w-most md:w-half">
     <h3 class="text-2xl mb-6">Today:</h3>
 
-    <div class="high-low-temps-container flex justify-between mb-6">
-      <div class="high-temp flex items-center justify-center">
-        <div class="h-6 w-6 mr-4">
-          <img src="arrow-up.png" alt="Up arrow." />
-        </div>
-        <span class={`text-${data.dailyHighTempColor} text-2xl`}>
-          <span class="high-temp">{data.dailyHighTemp}</span>
-          &deg;
-        </span>
-      </div>
-      <div class="low-temp flex items-center justify-center">
-        <div class="h-6 w-6 mr-4">
-          <img src="arrow-down.png" alt="Down arrow." />
-        </div>
-        <span class={`text-${data.dailyLowTempColor} text-2xl`}>
-          <span class="low-temp">{data.dailyLowTemp}</span>
-          &deg;
-        </span>
-      </div>
-    </div>
+    <HighLowTempsContainer
+      dailyHighTempColor={data.dailyHighTempColor}
+      dailyHighTemp={data.dailyHighTemp}
+      dailyLowTempColor={data.dailyLowTempColor}
+      dailyLowTemp={data.dailyLowTemp} />
 
-    <div class="sunrise-sunset-container flex justify-between">
-      <div class="sunrise flex items-center justify-center">
-        <div class="h-10 w-10 mr-2 md:mr-4">
-          <img src="sunrise.png" alt="Sun rising." />
-        </div>
-        <span class="sunrise-time text-base md:text-xl">
-          {data.sunriseTime}
-        </span>
-      </div>
-      <div class="sunset flex items-center justify-center">
-        <div class="h-10 w-10 mr-2 md:mr-4">
-          <img src="sunset.png" alt="Sun setting." />
-        </div>
-        <span class="sunset-time text-base md:text-xl">{data.sunsetTime}</span>
-      </div>
-    </div>
+    <SunriseSunsetContainer
+      sunriseTime={data.sunriseTime}
+      sunsetTime={data.sunsetTime} />
 
   </div>
 </div>
