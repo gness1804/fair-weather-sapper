a181886855ad3fb841e48504e14401e217ed0277
gness1804 <graham.nessler@gmail.com>
2019-10-14 16:28:21 -0500

    Add styling for the cities table

--- src/routes/[...loc]/custom.svelte
+++ src/routes/[...loc]/custom.svelte
@@ -21,57 +21,43 @@
   <title>{country.toUpperCase()}</title>
 </svelte:head>
 
-<div class="custom">
-  <p class="country-results-for-message">Results for {country.toUpperCase()}</p>
+<div class="custom-page">
+  <p class="country-results-for-message font-bold text-2xl text-center mb-6">
+    Results for {country.toUpperCase()}
+  </p>
 
   {#if countryData.length > 0}
-    {#if country.toUpperCase() === 'US'}
-      <table class="cities-result-table">
-        <thead>
-          <tr>
-            <th>Cities:</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Name</td>
+    <table class="cities-result-table border-2 border-gray-600 mx-auto my-0">
+      <thead class="bg-gray-400">
+        <tr>
+          <th
+            class="text-xl"
+            colspan={country.toUpperCase() === 'US' ? '4' : '3'}>
+            Cities:
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Name</td>
+          {#if country.toUpperCase() === 'US'}
             <td>State</td>
-            <td>Latitude</td>
-            <td>Longitude</td>
-          </tr>
-          {#each countryData as { name, state, lat, lng }}
-            <tr>
-              <td class="cities-result-table-name">{name}</td>
+          {/if}
+          <td>Latitude</td>
+          <td>Longitude</td>
+        </tr>
+        {#each countryData as { name, state, lat, lng }}
+          <tr>
+            <td class="cities-result-table-name">{name}</td>
+            {#if country.toUpperCase() === 'US'}
               <td class="cities-result-table-state">{state}</td>
-              <td class="cities-result-table-lat">{Number(lat).toFixed(2)}</td>
-              <td class="cities-result-table-lng">{Number(lng).toFixed(2)}</td>
-            </tr>
-          {/each}
-        </tbody>
-      </table>
-    {:else}
-      <table class="cities-result-table">
-        <thead>
-          <tr>
-            <th>Cities:</th>
+            {/if}
+            <td class="cities-result-table-lat">{Number(lat).toFixed(2)}</td>
+            <td class="cities-result-table-lng">{Number(lng).toFixed(2)}</td>
           </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Name</td>
-            <td>Latitude</td>
-            <td>Longitude</td>
-          </tr>
-          {#each countryData as { name, lat, lng }}
-            <tr>
-              <td class="cities-result-table-name">{name}</td>
-              <td class="cities-result-table-lat">{Number(lat).toFixed(2)}</td>
-              <td class="cities-result-table-lng">{Number(lng).toFixed(2)}</td>
-            </tr>
-          {/each}
-        </tbody>
-      </table>
-    {/if}
+        {/each}
+      </tbody>
+    </table>
   {:else}
     <p class="country-error-message text-red-600">
       Error: invalid country code. Please try again.
