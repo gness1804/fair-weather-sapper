ec7cdd99b921cb79471139d81bc84949c3a903b4
gness1804 <graham.nessler@gmail.com>
2019-10-15 13:11:55 -0500

    Move CitiesTable into own component

--- src/routes/[...loc]/custom.svelte
+++ src/routes/[...loc]/custom.svelte
@@ -33,6 +33,8 @@
 </script>
 
 <script>
+  import CitiesTable from '../../components/CitiesTable.svelte';
+
   export let countryData = [];
   export let country = 'Country Search';
   export let stateData = [];
@@ -57,40 +59,7 @@
   {/if}
 
   {#if countryData.length > 0}
-    <table class="cities-result-table border-2 border-gray-600 mx-auto my-0">
-      <thead class="bg-gray-400">
-        <tr>
-          <th class="text-xl" colspan={isAmerica ? '4' : '3'}>Cities:</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Name</td>
-          {#if isAmerica}
-            <td>State</td>
-          {/if}
-          <td>Latitude</td>
-          <td>Longitude</td>
-        </tr>
-
-        {#each countryData as { name, state, slug, geocoords: { lat, lng } }}
-          <tr>
-            <td class="cities-result-table-name">
-              <a
-                class="cities-result-table-name-link-wrapper"
-                href={`/cities/${slug}`}>
-                {name}
-              </a>
-            </td>
-            {#if isAmerica}
-              <td class="cities-result-table-state">{state}</td>
-            {/if}
-            <td class="cities-result-table-lat">{Number(lat).toFixed(2)}</td>
-            <td class="cities-result-table-lng">{Number(lng).toFixed(2)}</td>
-          </tr>
-        {/each}
-      </tbody>
-    </table>
+    <CitiesTable {isAmerica} data={countryData} />
   {:else}
     <p class="country-error-message text-red-600">
       Error: invalid country code. Please try again.
