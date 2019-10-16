441de1a1413384457ba2f9c084c9b33bd734ed35
gness1804 <graham.nessler@gmail.com>
2019-10-16 10:46:00 -0500

    Make seedCityData more robust by returning a promise

--- src/scripts/seedCityData.js
+++ src/scripts/seedCityData.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 /* eslint-disable no-await-in-loop */
 require('dotenv').config();
 const { writeFile } = require('fs');
@@ -15,6 +14,9 @@ const promisifiedWriteFile = util.promisify(writeFile);
 const promisifiedExec = util.promisify(exec);
 
 const seedData = async () => {
+  let errorCount = 0;
+  let errorMessage = 'There was a problem writing file data.';
+
   links = normalizeData([...links, ...extendedCities]);
   // eslint-disable-next-line no-unused-vars
   for (const link of links) {
@@ -25,18 +27,30 @@ const seedData = async () => {
 
     const file = `./src/routes/cities/_cityStaticData/${city}.json`;
 
-    const data = await axios.get(
-      `https://api.darksky.net/forecast/${process.env.DARK_SKY_KEY}/${lat},${lng}`,
-    );
-    await promisifiedWriteFile(file, JSON.stringify({ data: data.data }));
-    await promisifiedExec(`npm run prettier:spec ${file}`);
+    try {
+      const data = await axios.get(
+        `https://api.darksky.net/forecast/${process.env.DARK_SKY_KEY}/${lat},${lng}`,
+      );
+      await promisifiedWriteFile(file, JSON.stringify({ data: data.data }));
+      await promisifiedExec(`npm run prettier:spec ${file}`);
+    } catch (error) {
+      errorCount++;
+      errorMessage = error;
+    }
   }
-  return true;
+  return new Promise((resolve, reject) => {
+    if (errorCount) {
+      reject(errorMessage);
+    } else {
+      resolve('Successfully seeded city data.');
+    }
+  });
 };
 
 seedData()
-  .then(() => {
-    console.info('Successfully seeded city data.');
+  .then(message => {
+    // eslint-disable-next-line no-console
+    console.info(message);
   })
   .catch(err => {
     throw new Error(`Error with seeding city data: ${err}`);
