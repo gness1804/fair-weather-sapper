e7ed5989353d7f6074715ecfe08cc5669927d218
gness1804 <graham.nessler@gmail.com>
2019-10-22 11:15:31 -0500

    Add cypressRecord script

---
+++ src/scripts/cypressRecord.js
@@ -0,0 +1,22 @@
+const { execSync } = require('child_process');
+require('dotenv').config();
+
+/**
+ *
+ * @param {object} [error] - an error object
+ * @param {object} stdout - stdout object
+ * @param {object} stderr - stderr object
+ */
+const handleOutput = (error, stdout, stderr) => {
+  if (error) {
+    process.stderr.write(`Error: ${error.message || JSON.stringify(error)}`);
+    process.exit(1);
+  }
+  process.stdout.write(stdout);
+  process.stderr.write(stderr);
+};
+
+execSync(
+  `cypress run --record --key ${process.env.CYPRESS_RECORD_KEY}`,
+  handleOutput,
+);
